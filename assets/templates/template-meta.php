f04035f987c1edb43826a454212159c30bcfc44c
LittleGyaani <bmohanty@live.com>
2020-05-30 13:07:38 +0530

    Updated Meta Template

--- assets/templates/template-meta.php
+++ assets/templates/template-meta.php
@@ -5,18 +5,18 @@
 <!-- All Require Meta Tags Starts Here -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="keywords" content="Encore Education, Encore Bhubaneswar, Encore Group, Abroad Education Consultants, For Visa,  Study In United Kingdom (UK), Study In New Zealand, Study In Canada, Study In Australia, Study In USA, Study In Ireland,Study In Germany, Study In Singapore, Study In Malaysia, Foreign Universities and Educational Institutions, GRE, GMAT, IELTS, SAT, TOEFL, VISA APPLICATION Bhubaneswar, Study Abroad Bhubaneswar, MBBS Abroad Bhubaneswar, MBBS in China, MBBS in Bangladesh, MBBS in Philippines, AUPP, ATMC, Manipal Bhubaneswar, SARASWARIONLINE.com, Encore Consulting">
-<meta name="description" content="Encore Education Bhubaneswar is pioneered in providing pathway programs like Abroad Education, Overseas education, VISA Assistance, Foreign Studies, MBBS Abroad and only Foreign Education Company in Bhubaneswar which provides various courses for Studying Abroad like MBBS, partnered up with programs like ATMC, AUPP, Manipal and SARASWARIONLINE to make foreign studies easier and better at cheaper costs." />
+<meta name="keywords" content="Hotel Management Courses, Hotel Management Delhi, Hotel Management India, Study in India, hospitality management degree jobs, hospitality management degree salary, bachelor of hospitality management salary, bachelors in hospitality management salary">
+<meta name="description" content="LBIIHM is among the top hotel management colleges in Delhi that offers hotel management courses for students after 10th, 12th & after graduation. The main objective of the Hotel Management courses conducted here is to train the students in Technical, Managerial for the successful operation of a modern hotel." />
 <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="author" content="LittleGyaani - BRAHMA" />
-<meta name="topic" content="Overseas Education Consultants, Study Abroad, Study MBBS Abroad, VISA Provider Bhubaneswar, Education Consultants Bhubaneswar, Encore Education Bhubaneswar, Encore Bhubaneswar, Encore Group, Education At Abroad Bhubaneswar, MBBS ABROAD, AUPP, SARASWARIONLINE, ATMC, China, Bangladesh, Philippines, VISA APPLICATION ONLINE Bhubaneswar, Study In United Kingdom (UK), Study In New Zealand, Study In Canada, Study In Australia, Study In USA, Study In Ireland,Study In Germany, Study In Singapore, Study In Malaysia, Foreign Universities and Educational Institutions, GRE, GMAT, IELTS, SAT, TOEFL, VISA APPLICATION Bhubaneswar, Study Abroad Bhubaneswar, MBBS Abroad Bhubaneswar, MBBS in China, MBBS in Bangladesh, MBBS in Philippines, AUPP, ATMC, Manipal Bhubaneswar, SARASWARIONLINE.com, Encore Consulting" />
+<meta name="topic" content="Hotel Management Courses, Hotel Management Delhi, Hotel Management India, Study in India, hospitality management degree jobs, hospitality management degree salary, bachelor of hospitality management salary, bachelors in hospitality management salary" />
 <meta name="Search_Engines" content="Google, MSN, Bing, Overture, AltaVista, Yahoo, AOL, Infoseek, LookSmart, Excite, Hotbot, Lycos, Magellan, CNET, DogPile, Ask Jeeves, Teoma, Snap, Webcrawler" />
 <meta name='robots' content="INDEX, FOLLOW" />
 <meta name="YahooSeeker" content="INDEX, FOLLOW" />
 <meta name="msnbot" content="INDEX, FOLLOW" />
 <meta name="googlebot" content="INDEX, FOLLOW" />
-<meta name="copyright" content="(C) Encore Education - Encore Group." />
+<meta name="copyright" content="(C) Lakshya Bharati Institute of International Hotel Management." />
 <meta name="owner" content="bmohanty@live.com" />
 <meta name="allow-search" content="yes" />
 <meta name="app-url" content="<?= $baseURI; ?>" />
@@ -31,14 +31,14 @@
 <meta name="doc-type" content="Web Page">
 <meta name="doc-class" content="Published">
 <meta property="og:locale" content="en_US" />
-<meta property='og:title' content="Study Abroad, Study MBBS Abroad, Overseas Education Consultants in Bhubaneswar - Encore Education" />
+<meta property='og:title' content="Hospitality and Hotel Management Institute in India - LBIIHM" />
 <meta property='og:type' content="Website" />
 <meta property='og:url' content="<?= $baseURI; ?>" />
 <meta property="og:image" content="<?= $baseURI; ?>/assets/images/logo/lbiihm_logo.png" />
 <meta property='og:image:width' content="1200" />
 <meta property='og:image:height' content="630" />
 <meta property='og:image:type' content="image/png" />
-<meta property='og:description' content="Encore Education Bhubaneswar is pinoeered in providing pathway programs like Abroad Education, Overseas education, VISA Assistantce, Foregin Studies, MBBS ABROAD and only Foreign Education Company in Bhubaneswar which provides various courses for Studying Abroad like MBBS, partnered up with programs like ATMC, AUPP, Manipal and SARASWARIONLINE to make foregin studies easier and better at cheaper costs." />
+<meta property='og:description' content="LBIIHM is among the top hotel management colleges in Delhi that offers hotel management courses for students after 10th, 12th & after graduation. The main objective of the Hotel Management courses conducted here is to train the students in Technical, Managerial for the successful operation of a modern hotel." />
 
 <!-- Cannonical Link -->
 <link rel="canonical" href="<?= $baseURI; ?>" />
